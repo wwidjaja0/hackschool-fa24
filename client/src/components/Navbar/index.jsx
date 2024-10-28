2e98bdf4e1d18ef59934e2c4915f716d56575756
Angela Hu <angelahu925@gmail.com>
2024-10-28 15:24:58 -0700

    Update index.jsx

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -3,15 +3,9 @@ import Link from "next/link";
 import styles from "./style.module.css";
 
 const Navbar = () => {
-  return (
-    <div className={styles.navbar}>
-      <div className={styles.title}>
-        {/* Bonus: add a logo image with the Image tag that comes with Next.js: https://nextjs.org/docs/pages/api-reference/components/image */}
-      </div>
-      <div className={styles.navButtons}>
-      </div>
-    </div>
-  );
+  // Return your navbar code here
+  // Hint: use your navbar from the HTML workshop
+  // Bonus: you can add an image in the .title div for a logo
 };
 
 export default Navbar;
