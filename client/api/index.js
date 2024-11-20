89bb5a2aed40cb93632b4a9e689d1536d2ec19bf
widjaja <wwidjaja0@gmail.com>
2024-11-19 20:28:34 -0800

    fix api

--- client/api/index.js
+++ client/api/index.js
@@ -4,9 +4,9 @@ const serverURL = "https://hackschool-fa24-bkdt.onrender.com/";
 
 const TravelAPI = {
 	getTrip: () => axios.get(`${serverURL}/api/trip`),
-	getActivity: () => axios.get(`${serverURL}/api/activities`),
+	getActivity: () => axios.get(`${serverURL}/api/activity`),
 	postTrip: (payload) => axios.post(`${serverURL}/api/trip`, payload),
-	postActivity: (payload) => axios.post(`${serverURL}/api/activities`, payload),
+	postActivity: (payload) => axios.post(`${serverURL}/api/activity`, payload),
 };
 
 export default TravelAPI;
